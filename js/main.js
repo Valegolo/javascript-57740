b36f1c651cbaca8d5bd31c4c97d387549b7448a2
valegolo <valentinagolo98@hotmail.com>
2024-07-09 18:10:42 -0500

    update1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// comentario//
+let nombreDelusuario;
+let edadDelusuario;
+nomnbreDelusuario = "Lucas";
+edadDelusuario = "12";
+
+//Inicilizacion//
+let curso ="Javascript";
+const Comision = 6060
+let 
